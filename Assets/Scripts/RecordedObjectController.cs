6110a274ee2e26699300d6e2e1fc85c60873904a
quinton22 <quintonhoffman22@gmail.com>
2020-04-01 03:32:39 -0500

    working 3D application with recorded object position, scale, rotation

---
+++ Assets/Scripts/RecordedObjectController.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+// This class will record an objects position, scale, and rotation
+// It will also replay a recording depending on a switch
+public class RecordedObjectController : MonoBehaviour
+{
+    public class DataWrapper
+    {
+        public float? m_TimeStep = null;
+        public List<Vector3> m_PositionList = new List<Vector3>();
+        public List<Vector3> m_ScaleList = new List<Vector3>();
+        public List<Vector3> m_RotationList = new List<Vector3>();
+
+        // produces a json string
+        public string Serialize()
+        {
+            string str = "{";
+            str += $"'TimeStep':{m_TimeStep}\n";
+            str += $"'PositionList':{ListToString(m_PositionList)}\n";
+            str += $"'ScaleList':{ListToString(m_ScaleList)}\n";
+            str += $"'RotationList':{ListToString(m_RotationList)}";
+            str += "}";
+            
+            return str;
+        }
+
+        // creates object from json
+        public void Deserialize(string json)
+        {
+            json = json.Trim();
+            json = json.Trim(new Char[]{'{', '}'});
+
+            string[] jsonArray = json.Split('\n');
+
+            foreach (string obj in jsonArray)
+            {
+                string value = obj.Split(':')[1];
+
+                if (obj.Contains("TimeStep"))
+                {
+                    m_TimeStep = float.Parse(value);
+                }
+                else if (obj.Contains("Position"))
+                {
+                    m_PositionList = StringToList(value);
+                }
+                else if (obj.Contains("Scale"))
+                {
+                    m_ScaleList = StringToList(value);
+                }
+                else if (obj.Contains("Rotation"))
+                {
+                    m_RotationList = StringToList(value);
+                }
+            }
+        }
+
+        private string ListToString<T>(List<T> l)
+        {
+            return "[" + string.Join(";", l.Select(i => i.ToString())) + "]";
+        }
+
+        private List<Vector3> StringToList(string s)
+        {
+            List<Vector3> l = new List<Vector3>();
+
+            s = s.Substring(1, s.Length - 2);
+            string[] objArr = s.Split(';');
+            foreach (string obj in objArr)
+            {
+                l.Add(StringToObject(obj));
+            }
+
+            return l;
+        }
+
+        private Vector3 StringToObject(string s)
+        {
+            int index = s.IndexOf("(");
+
+            s = s.Substring(index + 1, s.Length - 2);
+            float[] p = (new List<string>(s.Split(','))).Select(i => float.Parse(i.Trim())).ToArray();
+            return new Vector3(p[0], p[1], p[2]);
+        }
+    }
+
+    private bool m_HasRecordingControllerBeenSet = false;
+    private RecordingController m_RecordingController;
+    public DataWrapper m_DataWrapper = new DataWrapper();
+    [HideInInspector]
+    public RecordingController.RecordMode m_RecordMode;
+    [HideInInspector]
+    public string textFileLocation;
+    private bool isRecording;
+    private int playbackIndex = 0;
+
+    public void SetRecordingController(RecordingController value)
+    {
+        if (!m_HasRecordingControllerBeenSet)
+        {
+            m_RecordingController = value;
+            m_HasRecordingControllerBeenSet = true;
+        }
+    }
+
+    void Start()
+    {
+        isRecording = m_RecordMode == RecordingController.RecordMode.On;
+
+        if (!isRecording)
+        {
+            Deserialize(System.IO.File.ReadAllText(textFileLocation + gameObject.name + ".txt"));
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            // TODO: turn off collider on object
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (isRecording)
+            System.IO.File.WriteAllText(textFileLocation + gameObject.name + ".txt", Serialize());
+    }
+
+    // This is better because it does not depend on frame rate... but (TODO) it may cause some perceived lag
+    // NOTE: The user should change the timestep in ProjectSettings > Time to 1/90 for VR
+    // TODO: this can be avoided by recording in FixedUpdate and playing back in Update but lerping based on the timestep
+    void FixedUpdate()
+    {
+        if (isRecording)
+        {
+            Record();
+        }
+        else 
+        {
+            Playback();
+        }
+    }
+
+    void Record()
+    {
+        if (m_DataWrapper.m_TimeStep == null)
+        {
+            m_DataWrapper.m_TimeStep = Time.deltaTime;
+        }
+
+        m_DataWrapper.m_PositionList.Add(transform.position);
+        m_DataWrapper.m_ScaleList.Add(transform.localScale);
+        m_DataWrapper.m_RotationList.Add(transform.rotation.eulerAngles);
+    }
+
+    void Playback()
+    {
+        if (playbackIndex >= m_DataWrapper.m_PositionList.Count) playbackIndex = 0;
+
+        transform.position = m_DataWrapper.m_PositionList[playbackIndex];
+        transform.localScale = m_DataWrapper.m_ScaleList[playbackIndex];
+        transform.rotation = Quaternion.Euler(m_DataWrapper.m_RotationList[playbackIndex]);
+
+        ++playbackIndex;
+    }
+
+    public string Serialize()
+    {
+       return m_DataWrapper.Serialize();
+    }
+
+    public void Deserialize(string json)
+    {
+        m_DataWrapper.Deserialize(json);
+    }
+}
---
+++ Assets/Scripts/RecordingController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class RecordingController : MonoBehaviour
+{
+
+    public enum RecordMode {
+        On,
+        Off
+    }
+
+    public RecordMode m_RecordMode;
+    //public List<GameObject> m_RecordedObjects;
+    //public List<RecordedObjectController> m_RecordedObjectControllers;
+    public string m_TextFileLocation;
+    [SerializeField]
+    private GameObject m_DisplayCanvas;
+
+    void Awake()
+    {
+        List<GameObject> m_RecordedObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag("RecordedObject"));
+        List<RecordedObjectController> m_RecordedObjectControllers = m_RecordedObjects.Select((GameObject obj) => obj.GetComponent<RecordedObjectController>()).ToList();
+
+        // set the m_RecordingController for every object
+        foreach (RecordedObjectController roc in m_RecordedObjectControllers)
+        {
+            roc.SetRecordingController(this);
+            
+            roc.m_RecordMode = m_RecordMode;
+            roc.textFileLocation = m_TextFileLocation;
+        }
+
+        if (m_RecordMode == RecordMode.Off && m_DisplayCanvas != null)
+        {
+            m_DisplayCanvas.transform.Find("RecordingImage").GetComponent<Image>().enabled = false;
+        }
+    }
+}
