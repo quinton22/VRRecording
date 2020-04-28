be271620153a8305a5b905cd935f1fa3ea7b7801
quinton22 <quintonhoffman22@gmail.com>
2020-04-28 02:41:05 -0500

    added VR support

--- Assets/Scripts/mSceneManager.cs
+++ Assets/Scripts/mSceneManager.cs
@@ -2,37 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
 
 public class mSceneManager : MonoBehaviour
 {
+    [System.Flags]
+    private enum PlatformType
+    {
+        AR,
+        Desktop,
+        VR
+    }
+
     [System.Serializable]
     private struct Activation
     {
         [SerializeField]
         GameObject obj;
+
         [SerializeField]
-        bool setActive;
-        public void Set()
+        [EnumFlagsAttribute]
+        private PlatformType activePlatforms;
+
+        public void Set(PlatformType currentPlatform)
         {
-            obj.SetActive(setActive);
+            obj.SetActive(ReturnSelectedElements().Contains((int) currentPlatform));
         }
-        
+
+        List<int> ReturnSelectedElements()
+        {
+
+            List<int> selectedElements = new List<int>();
+            for (int i = 0; i < System.Enum.GetValues(typeof(PlatformType)).Length; i++)
+            {
+                int layer = 1 << i;
+                if (((int)activePlatforms & layer) != 0)
+                {
+                    selectedElements.Add(i);
+                }
+            }
+
+            return selectedElements;
+        }
     }
 
     [System.NonSerialized]
     public bool isARActive;
+    [System.NonSerialized]
+    public bool isVRActive;
     [SerializeField]
     private Activation[] m_Activations;
     void Awake()
     {
         isARActive = Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
-        if (isARActive)
+        isVRActive = XRDevice.isPresent;
+
+        PlatformType platformType = isARActive ? PlatformType.AR : (isVRActive ? PlatformType.VR : PlatformType.Desktop);
+
+        Debug.Log($"Platform Type: {platformType.ToString()}");
+        
+        // activate correct objects
+        foreach (Activation activation in m_Activations)
         {
-            // load AR scene
-            foreach (Activation activation in m_Activations)
-            {
-                activation.Set();
-            }
+            activation.Set(platformType);
         }
+        
     }
 }
